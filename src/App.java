8362f8a403fb251559bbfd401123e8635dcc6f61
Wictor Rafael <wictor.santos20@gmail.com>
2023-06-03 21:37:26 -0300

    mentoria e curso

--- src/App.java
+++ src/App.java
@@ -1,5 +1,26 @@
+import java.time.LocalDate;
+
+import desafio_dominio.Curso;
+import desafio_dominio.Mentoria;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descricao do curso java");
+        curso1.setCargaHoraria(10);
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Javascript");
+        curso2.setDescricao("descricao do curso js");
+        curso2.setCargaHoraria(8);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
     }
 }
